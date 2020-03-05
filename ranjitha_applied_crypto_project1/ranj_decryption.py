f216e5e1fcd98fed8d74b025347fb2d4db4849df
Ranjitha Gurushanthappa <ranjitha.gurushanth@gmail.com>
2020-03-04 21:26:50 -0500

    Update decryption code

--- ranjitha_applied_crypto_project1/ranj_decryption.py
+++ ranjitha_applied_crypto_project1/ranj_decryption.py
@@ -16,6 +16,7 @@ def computeLPSArray(string, M, lps):
                 lps[i] = 0
                 i += 1
 
+
 # returns true if string is repetition of one of its substrings; else return false.
 def isRepeat(string):
     n = len(string)
@@ -27,61 +28,83 @@ def isRepeat(string):
     else:
         False
 
+# checks if key is valid depending on repititions
+def check_key_validity(key):
+    done = False
+    n = len(key)
+    for i in range(1,25):
+        if isRepeat(key[:n-i]):
+            #print(key[:n-i])
+            return key[:n-i]
+    return None
+#print(check_key_validity("ae gmae gmae gmae"))
+
+
+# gives indicies from the alphabet for all the letters in the input string passed as arg
+def get_number_for_letters(text):
+    letters = " abcdefghijklmnopqrstuvwxyz"
+    number_array_for_text = []
+    
+    for i in range(0, len(text)):
+        done = False
+        j = 0
+        while not done:
+            if text[i] == letters[j]:
+                number_array_for_text.append(j)
+                done = True
+            else:
+                j+=1
+    
+    return number_array_for_text
+
+
+# compares ciphertext with plaintext and returns the key
 def compare_cipher_with_plaintext(ciphertext, plaintext):
-    # ord is a function in python which returns the ascii value of the character
-    # the following two lines of code gives an array of ascii values for both plaintext and ciphertext
-    ords_plaintext = [ord(c) for c in plaintext]
-    ords_ciphertext = [ord(c) for c in ciphertext]
+    letters = " abcdefghijklmnopqrstuvwxyz"
     key_shifts = []
-    for i in ords_plaintext:
-        if i == 32:
-            
-        key_shifts = key_shifts.append(ords_ciphertext[i] - ords_plaintext[i]
-
-if __name__ == "__main__":
-    pass
-    five_plaintext_file = open("plaintext_dictionary_test1.txt", "r")
-    word_dictionary_file = open("word_dictionary_test2.txt", "r")
-    five_plaintext_file_content = five_plaintext_file.read()
-    word_dictionary_file_content = word_dictionary_file.read()
     
-
-'''
-def limits_correction(character, distance, start, end):
-    char = character
-    if char >= start and char < end:
-        if char + distance >= end:
-            char = char + distance - 26
+    ciphertext_array = get_number_for_letters(ciphertext)
+    plaintext_array = get_number_for_letters(plaintext)
+    
+    for i in range(len(ciphertext_array)):
+        if plaintext_array[i] > ciphertext_array[i]:
+            n = 26 - plaintext_array[i]
+            m = ciphertext_array[i]
+            shift = n+m+1
         else:
-            char = char + distance
-    return char
-
-def modify_string(string, distance):
-    # string here is the plaintext that we need to shift
-    ords = [ord(c) for c in string]
-
-    corrected_distance = 0
-    if distance > 26:
-        corrected_distance = distance % 26
-    elif distance > 0 and distance <= 26:
-        corrected_distance = distance
-
-    lower_start = 97
-    lower_end = lower_start + 26
-
-    shifted_string = []
-
-    for char in ords:
-        if char >= lower_start and char < lower_end:
-            char = limits_correction(char, corrected_distance, lower_start, lower_end)
-        shifted_string.append(chr(char))
+            shift = ciphertext_array[i] - plaintext_array[i]
+        key_shifts.append(shift)
     
-    print(''.join(shifted_string))
-    return ''.join(shifted_string)
+    key = []
+    for shift in key_shifts:
+        key.append(letters[shift])
 
-modify_string("abc", 28)
-'''
+    return ("".join(key))
+#compare_cipher_with_plaintext("jeatmwjremtyrletjd", "i am very stressed")
 
-    
-    
+
+def guess(ciphertext, plaintext):
+    key = compare_cipher_with_plaintext(ciphertext, plaintext)
+    bool = check_key_validity(key)
+    if bool == True:
+        return bool
+
+
+if __name__ == '__main__':
+    #five_plaintext_file = open("plaintext_dictionary_test1.txt", "r")
+    #word_dictionary_file = open("word_dictionary_test2.txt", "r")
+    #five_plaintext_file_content = five_plaintext_file.read()
+    #word_dictionary_file_content = word_dictionary_file.read()
+    dict = {
+    0: "gunfights outjuts molters forgot bedclothes cirrus servomotors tumulus incompleteness provoking sixteens breezeways layoff marinas directives teabowl vugs mainframe gazebo bushwhacks testers incompressibility unthoughtfully rivalled repaint nonuple guerre semiaquatic flashgun esthetics icefall touchups baltic baba gorget groper remittances nimbus podium reassurance preventable overroasts chests interchangeable pentarch doctoring potentiated salts overlay rustled recyclability version mottled lee",
+    1: "intersectional marquees undeniably curates papa invidiousness libidinal congratulate annexion stompers oxblood relicense incept viny dimers typicality meteors indebtedness triceratops statisms arsenides horsed melanin smelt ulsters films townfolk orchestrations disintoxication ceiled allegories pinsetters misdeliveries firebreak baronages sphere stalest amino linkboy plasm avers cocktail reconfirming rearoused paternity moderation pontificated justices overplays borzois trailblazers smelters cor",
+    2: "frosteds shelters tannest falterer consoles negroes creosote lightful foreshadow mustangs despatches unofficially sanitarium single integrates nebula del stubby impoliteness royal ariel triceratops episcopalians pensive passports largesses manwise repositioned specified promulgates polled fetus immune extinguisher paradise polytheist abdicated ables exotica redecorating embryological scintillatingly shysters parroted twosomes spermicide adapters illustrators suffusion bonze alnicoes acme clair p",
+    3: "distributee hermitage talmudic thruput apologues recapitulate keyman palinodes semiconscious fauns culver evicts stubbornness stair virginals unto leonardo lyrist merci procuration repulsing medicated lagoons cohort caravans pampas maundered riggings undersell investigator arteriolar unpolled departmentalization penchants shriveled obstreperous misusing synfuels strewn ottawas novelising cautiously foulmouthed travestied bifurcation classicists affectation inverness emits admitter bobsledded erg",
+    4: "undercurrents laryngeal elevate betokened chronologist ghostwrites ombres dollying airship probates music debouching countermanded rivalling linky wheedled heydey sours nitrates bewares rideable woven rerecorded currie vasectomize mousings rootstocks langley propaganda numismatics fucked subduers babcock jauntily ascots nested notifying mountainside dirk chancellors disassociating eleganter radiant convexity appositeness axonic trainful nestlers applicably correctional stovers organdy bdrm insis"}
+    cipher_input = input("Please input ciphertext to be decrypted: ")
+    for i in range(5):
+        bool = guess(cipher_input, dict[i])
+        #print(bool)
+        if bool is True:
+            print("The correct guess is: ", dict[i])
 
