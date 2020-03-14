cffceeffb54be76d5c6aa66076cc7719cfe946b1
Ranjitha Gurushanthappa <ranjitha.gurushanth@gmail.com>
2020-03-14 17:48:17 -0400

    Fully functioning Kasiski code

--- AppCrypt1/Kasiski.py
+++ AppCrypt1/Kasiski.py
@@ -1,7 +1,7 @@
 import itertools
 
-englishLetterFreq = {'e': 12.70, 't': 9.06, 'a': 8.17, 'o': 7.51, 'i': 6.97, 'n': 6.75, 's': 6.33, 'h': 6.09, 'r': 5.99, 'd': 4.25, 'l': 4.03, 'c': 2.78, 'u': 2.76, 'm': 2.41, 'w': 2.36, 'f': 2.23, 'g': 2.02, 'y': 1.97, 'p': 1.93, 'b': 1.29, 'v': 0.98, 'k': 0.77, 'j': 0.15, 'x': 0.15, 'q': 0.10, 'z': 0.07}
-ETAOIN = 'etaoinshrdlcumwfgybvkjxqz'
+englishLetterFreq = {'e': 12.02, 't': 9.1, 'a': 8.12, 'o': 7.68, 'i': 7.31, 'n': 6.95, 's': 6.28, 'r': 6.02, 'h': 5.92, 'd': 4.32, 'l': 3.98, 'u': 2.88, 'c': 2.71, 'm': 2.61, 'f': 2.30, 'y': 2.11, 'w': 2.09, 'g': 2.03, 'p': 1.82, 'b': 1.49, 'v': 1.11, 'k': 0.69, 'x': 0.17, 'q': 0.11, 'j': 0.10, 'z': 0.07}
+ETAOIN = 'etaoinsrhdlucmfywgpbvkxqjz'
 LETTERS = ' abcdefghijklmnopqrstuvwxyz'
 NUM_MOST_FREQ_LETTERS = 24 # attempts th# is many letters per subkey
 MAX_KEY_LENGTH = 30 # will not attempt keys longer than this
