0efa755461a79384fbe7f1d101c5532e4eeb84b2
Ridhima Agarwal <ridhimaagarwal@ridhimas-mbp.home>
2020-03-14 20:11:50 -0400

    Updated functions

--- ridhima/test2.cpp
+++ ridhima/test2.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 LETTERS = " abcdefghijklmnopqrstuvwxyz";
 ETAOIN = "etaoinsrhdlucmfywgpbvkxqjz";
-NUM_MOST_FREQ_LETTERS = 10;
-MAX_KEY_LENGTH = 10;
+NUM_MOST_FREQ_LETTERS = 10; //attempts this many letters per subkey
+MAX_KEY_LENGTH = 10; //will not attempt keys longer than this
 DICTIONARY2 = ['awesomeness', 'hearkened', 'aloneness', 'beheld', 'courtship', 'swoops', 'memphis', 'attentional', 'pintsized', 'rustics', 'hermeneutics', 'dismissive', 'delimiting', 'proposes', 'between', 'postilion', 'repress', 'racecourse', 'matures', 'directions', 'pressed', 'miserabilia', 'indelicacy', 'faultlessly', 'chuted', 'shorelines', 'irony', 'intuitiveness', 'cadgy', 'ferries', 'catcher', 'wobbly', 'protruded', 'combusting', 'unconvertible', 'successors', 'footfalls', 'bursary', 'myrtle', 'photocompose', ' ', '']
 
 
@@ -49,6 +49,8 @@ vector <char> as_list(string key) {
 }
 
 map <char,int> getLetterCount(string message) {
+	// Returns a dictionary with keys of single letters and values of the
+    // count of how many times they appear in the message parameter.
 	map <char,int> lettercount = {{'a', 0},{'b', 0}, {'c', 0}, {'d', 0}, {'e', 0}, {'f', 0}, {'g', 0}, {'h', 0}, {'i', 0}, {'j', 0}, {'k', 0}, {'l', 0}, {'m', 0}, {'n', 0}, {'o', 0}, {'p', 0}, {'q', 0}, {'r', 0}, {'s', 0}, {'t', 0}, {'u', 0}, {'v', 0}, {'w', 0}, {'x', 0}, {'y', 0}, {'z', 0}, {' ',0};
 	int ind =0;
 	for(int l=0; l<strlen(message); l++) {
@@ -67,11 +69,18 @@ string getFrequencyOrder(string message) {
 int englishFreqMatchScore(string message) {
 	string freqOrder = getFrequencyOrder(message);
 	int matchScore =0;
-	
+
 }
 
 map <string,vector<int>> findRepeatSequences(string message) {
 	// remove non letters and spaces from the message?
+	// Goes through the message and finds any 3 to 5 letter sequences
+    //that are repeated. Returns a dict with the keys of the sequence and
+    //values of a list of spacings (num of letters between the repeats).
+    //Use a regular expression to remove non-letters from the message.
+    //message = NONLETTERS_PATTERN.sub('', message.lower())
+
+    // Compile a list of seqLen-letter sequences found in the message.
 	map <string, vector <int> > seqSpace; //keys are sequences, values are list of spaces between them 
 	map <string,vector<int>>::iterator it;
 	string sequence ="";
@@ -94,6 +103,9 @@ map <string,vector<int>> findRepeatSequences(string message) {
 
 vector <int> getUsefulFactors(int num) {
 	//returns a list of factors of a number. each factor should be at max 24
+	// Returns a list of useful factors of num. By "useful" we mean factors
+    // less than MAX_KEY_LENGTH + 1. For example, getUsefulFactors(144)
+    // returns [2, 72, 3, 48, 4, 36, 6, 24, 8, 18, 9, 16, 12]
 	vector <int> factors;
 	if num<2 return factors;
 	for(int i=2;i<25;i++){
@@ -109,9 +121,13 @@ vector <int> getUsefulFactors(int num) {
 }
 
 vector <tuple<int,int>> MostCommonfactors(map <string,vector<int>> seqfactors) {
+	//First, get a count of how many times a factor occurs in seqFactors.
 	map<int,int> factorcounts;
 	map<int,int> iterator it2 = factorcounts.begin();
 	map <string, vector<int>>::iterator it1 = seqfactors.begin();
+	//seqFactors keys are sequences, values are lists of factors of the
+     // spacings. seqFactors has a value like: {'GFD': [2, 3, 4, 6, 9, 12,
+     //18, 23, 36, 46, 69, 92, 138, 207], 'ALW': [2, 3, 4, 6, ...], ...}
 	while(it1!=seqfactors.end()){
 		vector<int> factorList=it1->second;
 		for(int factor=0;factor<factorList.size();factor++) {
@@ -148,6 +164,9 @@ vector <tuple<int,int>> MostCommonfactors(map <string,vector<int>> seqfactors) {
 }
 
 vector <int> kasiski(string ciphertext){
+	/* # Find out the sequences of 3 to 5 letters that occur multiple times
+     # in the ciphertext. repeatedSeqSpacings has a value like:
+     # {'EXG': [192], 'NAF': [339, 972, 633], ... } */
 	repeatSpacings = findRepeatSequences(ciphertext); 
 	map <string,vector<int>> seqfactors; //factors of each number in the sequences list
 	map <string, vector<int>>::iterator it = repeatSpacings.begin();
@@ -172,6 +191,14 @@ vector <int> kasiski(string ciphertext){
 
 string getNthSubkeysLetters(int n, int keylength, string message ) {
 	//remove non letters from the message
+	/* Returns every Nth letter for each keyLength set of letters in text.
+     # E.g. getNthSubkeysLetters(1, 3, 'ABCABCABC') returns 'AAA'
+     #      getNthSubkeysLetters(2, 3, 'ABCABCABC') returns 'BBB'
+     #      getNthSubkeysLetters(3, 3, 'ABCABCABC') returns 'CCC'
+     #      getNthSubkeysLetters(1, 5, 'ABCDEFGHI') returns 'AF'
+
+     # Use a regular expression to remove non-letters from the message.
+     #message = NONLETTERS_PATTERN.sub('', message) */
 	int i =n-1;
 	string letter = "";
 	vector <char> letters;
@@ -185,10 +212,14 @@ string getNthSubkeysLetters(int n, int keylength, string message ) {
 }
 
 string attemptHackWithKeyLength(string ciphertext, int mostLikelyKeyLength) {
+	// Determine the most likely letters for each letter in the key.
+    // allFreqScores is a list of mostLikelyKeyLength number of lists.
+    // These inner lists are the freqScores lists.
 	vector <vector<int>> allFreqScores;
 	for (int nth=1; nth <mostLikelyKeyLength+1; nth++) {
 		string nthLetters = getNthSubkeysLetters(nth, mostLikelyKeyLength, ciphertext);
-		vector <tuple<char,int>> freqScores;
+		vector <tuple<char,int>> freqScores; 
+		//freqScores is a list of tuples like: [(<letter>, <Eng. Freq. match score>), ... ]
 
 	}
 }
